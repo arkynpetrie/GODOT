f3d19a9cb1688aaa18e58d541622f3e95fc6fce6
arkynpetrie <arkpet90@kingshigh.school.nz>
2022-08-01 20:06:53 +1200

    done stuff

--- Godot/CollisionShape2D.gd
+++
@@ -1,9 +0,0 @@
-extends CollisionShape2D
-
-func _ready():
-	$AnimationPlayer.play("SpikeTrigger")
-
-
-func _on_Area2D_body_entered(body):
-	if body.is_in_group("Player"):
-		print("player dead")
--- Godot/SpikeTrap.gd
+++ Godot/SpikeTrap.gd
@@ -1,11 +1,9 @@
-extends Node2D
+extends Area2D
 
 func _ready():
-	$AnimationPlayer.play("Swing")
-	pass
-
-
-
+	$AnimationPlayer.play("SpikeTrigger")
+	
+	
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
 		print("player dead")
