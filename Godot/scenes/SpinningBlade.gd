bb037d888bdf9f34ebf9043c00d8cc28db03f12b
arkynpetrie <arkpet90@kingshigh.school.nz>
2022-08-04 21:00:51 +1200

    da stuff

--- Godot/scenes/SpinningBlade.gd
+++ Godot/scenes/SpinningBlade.gd
@@ -1,7 +1,6 @@
 extends Node2D
 export (int) var follow_speed = 50
-func _ready():
-	$AnimationPlayer.play("spin")
-	
+
+
 func _process(delta):
-	$Path2D/PathFollow2D.offset += follow_speed * delta 
+	$Path2D/PathFollow2D.offset += follow_speed * delta
