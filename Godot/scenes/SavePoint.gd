7f96552d4f99c9593bc0051ca1dd69262fa1d11e
arkynpetrie <arkpet90@kingshigh.school.nz>
2022-08-05 13:25:30 +1200

    nearly done the checkpoint stuff

---
+++ Godot/scenes/SavePoint.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+func _ready():
+	$AnimationPlayer.play("Checkpoint")
+	
+func _process(delta):
+	if GameStats.get_spawn() != self:
+		$AnimationPlayer.play("Checkpoint")
+	
+
+
+
+func _on_SavePoint_body_entered(body):
+	if body.is_in_group("Player"):
+		GameStats.set_spawn(self)
+		$AnimationPlayer.play("saved")
