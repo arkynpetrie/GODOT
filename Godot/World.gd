c7de30e8f72fd1bc0b52346945cf4a255b0aa6dd
arkynpetrie <arkpet90@kingshigh.school.nz>
2022-08-11 10:58:58 +1200

    finished the timer code

--- Godot/World.gd
+++ Godot/World.gd
@@ -4,6 +4,14 @@ func _ready():
 	pass
 
 func _process(delta):
-	pass
+
 	if Input.is_action_just_pressed("reset"):
-		GameStats.reset()	
+		GameStats.reset()
+	print(GameStats.get_time())
+	$CanvasLayer/Control/Label.text = GameStats.get_time()
+
+
+
+
+func _on_Area2D_body_entered(body):
+	pass
