13cdffc279b7292d2b0f9e37806f886b870c7006
arkynpetrie <arkpet90@kingshigh.school.nz>
2022-08-18 09:58:33 +1200

    Fix some stuff with the deadly and the node.

--- Godot/scenes/Player.gd
+++ Godot/scenes/Player.gd
@@ -79,7 +79,6 @@ func _physics_process(delta):
 func _on_DeathZone_area_entered(area):
 	pass # Replace with function body.
 
-func _on_DeathZone_area_entered(area):
 	if area.is_in_group("Deadly"):
-		if GameStats.check_reset() == flase:
+		if GameStats.check_reset() == false:
 			global_position = GameStats.get_spawn().global_position
