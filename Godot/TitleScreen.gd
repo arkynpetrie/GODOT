b9e70084be884532f8b4f66939bc5553a9aaa3e6
arkynpetrie <arkpet90@kingshigh.school.nz>
2022-10-27 21:55:10 +1300

    Did intro and tips
    Finished most of the title screen need to add a options menu and a quit button and 1 trap and add a finish pole

---
+++ Godot/TitleScreen.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+
+func _ready():
+	pass 
+
+
+func _on_NewGameButton_pressed():
+	get_tree().change_scene("res://World.tscn")
+	
+
+
+func _on_OptionsMenuButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	pass # Replace with function body.
